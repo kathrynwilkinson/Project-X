cb4778117636b3431d3bdf25ead7af10eb92f36d
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2022-03-26 20:20:08 -0500

    Add styled components to Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,14 @@
-import './style.css';
 import React from 'react';
+import './style.css';
+import Contact from '../Contact';
+import Creator from '../Creator';
 
-export default function Footer() {
-	return (
-		<div>Footer</div>
-	);
+export default function Footer () {
+    return (
+
+        <footer>
+            <Contact />
+            <Creator />
+        </footer>
+    );
 }
