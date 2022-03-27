250d269fb4c86a5e81c6db5267cb87e1f5faf034
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2022-03-26 21:49:08 -0500

    Add projects to Gallery page, style positioning

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,8 +1,20 @@
 import './style.css';
 import React from 'react';
 
-export default function ProjectCard() {
+export default function ProjectCard({gitURL, liveURL, title, id}) {
 	return (
-		<div>Project Card</div>
+		<div className='project-card'>
+			<a href={gitURL} target='_blank' rel='noreferrer'>
+                <div title=' View Code ' className="thumbnail code-thumb" id={id + '1'}>
+                    <div className='screen code-screen'></div>
+                </div>
+            </a>
+            <h5>{title}</h5>
+            <a href={liveURL} target='_blank' rel='noreferrer'>
+                <div title=' View Live ' className="thumbnail live-thumb" id={id + '2'}>
+                    <div className='screen live-screen'></div>
+                </div>
+            </a>
+		</div>
 	);
 }
--- src/pages/Gallery/index.js
+++ src/pages/Gallery/index.js
@@ -1,13 +1,14 @@
 import './style.css';
 import React from 'react';
 import ProjectCard from '../../components/ProjectCard';
+import projects from '../../data/projects.json';
 
 export default function Gallery() {
 	return (
 		<div className='page-body'>
 			<div className='page-header'>Gallery</div>
-			<div>ProjectCard section
-				<ProjectCard />
+			<div className='portfolio-sec'>
+				{projects.map( project => ( <ProjectCard {...project} key={project.id}/>)) }
 			</div>
 		</div>
 	);
