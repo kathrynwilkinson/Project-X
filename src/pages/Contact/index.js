f853b901bb96a6e5c92b03fe42e386e24113fbfe
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2022-03-30 00:48:35 -0500

    Add background images to folder, add background images to contact page + style, link + style google fonts

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -2,13 +2,21 @@ import './style.css';
 import React from 'react';
 import ContactLinks from '../../components/ContactLinks';
 import contactBaby from '../../images/contact/cool-baby-square.PNG';
+import cloudFade from '../../images/cloudfade.JPG';
 
 export default function Contact() {
 	return (
 		<div className='page-body' id='contact-page'>
 			<div className='page-header'>contact.</div>
 			<ContactLinks />
-			<img className='contact-baby' alt='' src={contactBaby}/>
+			<div className='flex-wrapper'>
+				<h3 id='contact-blurb'>Have you ever had a dream, that you were so sure was real? <br />
+					What if you were unable to wake from that dream? <br />
+					How would you know the difference between the dream world and real world?</h3>
+				<img id='contact-baby' alt='' src={contactBaby}/>
+			</div>
+
+			<img className='fade-img' alt='' src={cloudFade}/>
 		</div>
 	);
 }
